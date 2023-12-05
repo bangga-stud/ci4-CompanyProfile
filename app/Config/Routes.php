f2b8f572994268991b2e6a26a34891fab3b454e9
bangga-stud <bangga.angkasa@ummi.ac.id>
2023-12-06 02:58:08 +0700

    create routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'LandingPageID::home');
+$routes->get('/tentang-kami', 'LandingPageID::about');
+$routes->get('/galeri', 'LandingPageID::galery');
+$routes->get('/hubungi-kami', 'LandingPageID::contact');
+$routes->get('/bisnis-kami', 'LandingPageID::business');
+$routes->get('/penambangan', 'LandingPageID::minning');
+$routes->get('/pengriman-tongkang', 'LandingPageID::forwarding');
---
+++ app/Controllers/LandingPageID.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controllers;
+
+class LandingPageID extends BaseController
+{
+    public function home(): string
+    {
+        return view('welcome_message');
+    }
+
+    public function about(): string
+    {
+        return view('welcome_message');
+    }
+
+    public function galery(): string
+    {
+        return view('welcome_message');
+    }
+
+    public function contact(): string
+    {
+        return view('welcome_message');
+    }
+
+    public function business(): string
+    {
+        return view('welcome_message');
+    }
+
+    public function minning(): string
+    {
+        return view('welcome_message');
+    }
+
+    public function forwarding(): string
+    {
+        return view('welcome_message');
+    }
+}
